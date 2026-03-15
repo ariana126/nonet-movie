d53eac22e5710691dcff0eb2dce36fe2ee1cac53
Ariana Maghsoudi <ariana.maghsoudi82@gmail.com>
2026-03-15 14:14:55 +0330

    feat: Add logging

--- src/nonet_movie/infrastructure/boot.py
+++ src/nonet_movie/infrastructure/boot.py
@@ -1,3 +1,8 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from pythonjsonlogger import json
 from dotenv import load_dotenv
 from pydm import ServiceContainer, EnvParametersBag
 
@@ -11,11 +16,12 @@ from ..domain.service.movie_repositoy import MovieRepository
 from ..domain.service.series_repository import SeriesRepository
 
 
-def boot():
+def boot() -> None:
     service_container = ServiceContainer.get_instance()
 
     load_dotenv()
-    service_container.set_parameters(EnvParametersBag())
+    parameters = EnvParametersBag()
+    service_container.set_parameters(parameters)
 
     service_container.bind_parameters(JsonDB, {'db_path': 'JSON_DB_PATH'})
 
@@ -24,3 +30,23 @@ def boot():
 
     service_container.bind(MovieSourcesFactory, MovieSourcesFactoryImpl)
     service_container.bind(SeriesSourcesFactory, SeriesSourcesFactoryImpl)
+
+    configure_logger(parameters.get('LOG_PATH'))
+
+
+def configure_logger(log_path: str) -> None:
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.INFO)
+    formatter = json.JsonFormatter(
+        "%(asctime)s %(levelname)s %(name)s %(message)s"
+    )
+    handler = RotatingFileHandler(
+        f'{log_path}/app.log',
+        maxBytes=5_000_000,
+        backupCount=5
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
